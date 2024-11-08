abec88201d251551ac38ea4bf5a3d72f4092d7d9
BlessedUzama <uzamablessed@gmail.com>
2024-11-09 00:11:37 +0100

    third attempt task3

--- my-company/src/components/NavBar.jsx
+++ my-company/src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='About'>About</Link>
-      <Link to='Contact'>Contact</Link>
-      <Link to='Services'>Services</Link>
+      <Link to='/' />
+      <Link to='About' />
+      <Link to='Contact' />
+      <Link to='Services' />
     </nav>
   );
 }
