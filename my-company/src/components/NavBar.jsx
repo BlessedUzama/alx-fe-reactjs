6dbf5952b0d66a61ed3173413ec2a724a2d593fd
BlessedUzama <uzamablessed@gmail.com>
2024-11-09 00:14:09 +0100

    fourth attempt task3

--- my-company/src/components/NavBar.jsx
+++ my-company/src/components/NavBar.jsx
@@ -3,11 +3,27 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to='/' />
-      <Link to='About' />
-      <Link to='Contact' />
-      <Link to='Services' />
+    <nav
+      style={{
+        backgroundColor: "#121212",
+        display: "block",
+        justifyContent: "center",
+      }}
+    >
+      <ul>
+        <li>
+          <Link to="/"> Home </Link>
+        </li>
+        <li>
+          <Link to="/about"> About </Link>
+        </li>
+        <li>
+          <Link to="/services"> Services </Link>
+        </li>
+        <li>
+          <Link to="/contact"> Contact </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
