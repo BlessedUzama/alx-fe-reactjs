1350dacf6f043be5612859b01c7b2f7a00c6634d
BlessedUzama <uzamablessed@gmail.com>
2024-11-21 15:54:23 +0100

    1st attempt task2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,14 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
+import ProtectedRoute from './components/ProtectedRoute';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import BlogPost from './components/BlogPost';
 
 function App () {
 
   return (
-    <BrowserRouter>
+    <Routes>
       <Routes>
-        <Route path="/profile" element={Profile} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+        <Route path="/blog/:id" element={<BlogPost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </Routes>
   );
 }
 
