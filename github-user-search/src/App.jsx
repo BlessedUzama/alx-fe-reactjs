452ce915ea1820d5867bc1971791c4f1eb819d54
BlessedUzama <uzamablessed@gmail.com>
2024-12-12 02:42:02 +0100

    1st attempt task0

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,18 +1,14 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Search from "./components/Search";
+
+import './App.css'
 
 function App() {
+  
+
   return (
     <>
-      <div className="min-h-screen  text-[#f8f9fa] flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold mb-8 text-[#6c757d]">
-          Search GitHub Like a Pro!
-        </h1>
-        <Search />
-      </div>
+      <h1>Github Search</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
