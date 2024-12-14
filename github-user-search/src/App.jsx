97191bb125f04fecd9780fe64d83c4537bc15d85
BlessedUzama <uzamablessed@gmail.com>
2024-12-14 03:23:57 +0100

    test

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -7,11 +7,14 @@ function App() {
 
   return (
     <>
-      <h1>Github Search</h1>
-      <Search /> 
-      
+      <div className="min-h-screen  text-[#f8f9fa] flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold mb-8 text-[#6c757d]">
+          Search GitHub Like a Pro!
+        </h1>
+        <Search />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
